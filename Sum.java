eebd9722c77089a4ae9bf79e5c02a55aded8bf81
kabeershaikhh <shaikhkabeer069@gmail.com>
2024-08-13 18:27:54 +0500

    Initial Files

---
+++ Sum.java
@@ -0,0 +1,14 @@
+package LocalRepo;
+
+public class Sum {
+    public static void sum(int a,int b){
+        int r=a+b;
+        System.out.println("sum is: "+r);
+    }
+    public static void main(String[] args) {
+        
+        sum(4,6);
+    } 
+    
+    
+}
